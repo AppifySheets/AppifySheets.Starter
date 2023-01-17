db1cccbd6a534a13a4877c1ed9eb860666ade601
Petre <petrechitashvili@users.noreply.github.com>
2023-01-17 15:35:37 +0400

    Added ChocoInstaller

---
+++ ChocoInstaller/choco-installer.cmd
@@ -0,0 +1,9 @@
+choco feature enable -n allowGlobalConfirmation
+
+choco install docker-desktop
+choco install dbeaver
+choco install visualstudio2022community
+choco install docker-compose
+choco install vscode
+
+pause
